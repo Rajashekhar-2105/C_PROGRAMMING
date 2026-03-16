ceed3f0f329f29af9ec7ed0de54e4504a73fae14
Rajashekhar-2105 <rajubiradar2105>
2026-03-17 01:31:35 +0530

    modified a value and print that value

--- POINTERS/prg1.c
+++ POINTERS/prg1.c
@@ -6,4 +6,7 @@ void main()
     printf("Value of a is : %d\n",a);
     printf("The value of a is : %d\n",*ptr);
     printf("The address of a is : %p\n",&a);
+    *ptr = 25;
+    printf("Value of a is : %d\n",a);
+    printf("The value of a is : %d\n",*ptr);
 }
