23cb22cfbde3aac018d9301154e3f6f59002aefd
Rajashekhar-2105 <rajubiradar2105>
2026-03-17 01:29:40 +0530

    added one more printf to print a value

--- POINTERS/prg1.c
+++ POINTERS/prg1.c
@@ -3,6 +3,7 @@ void main()
 {
     int a=10;
     int* ptr = &a;
+    printf("Value of a is : %d\n",a);
     printf("The value of a is : %d\n",*ptr);
     printf("The address of a is : %p\n",&a);
 }
