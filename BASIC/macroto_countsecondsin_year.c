37ddbac0b35f9d0198be8e438e3f8a344bf18f54
Rajashekhar-2105 <rajubiradar2105@gmail.com>
2026-05-13 15:51:30 +0530

    added code to count seconds in one year

---
+++ BASIC/macroto_countsecondsin_year.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define Per_min_sec 60
+#define Per_hour_min 60
+#define hou_per_day 24
+#define Day_per_year 365
+#define Sec_in_one_year (60*60*24*365)
+void main()
+{
+    printf("seconds in one year = %d",Sec_in_one_year);
+
+}
